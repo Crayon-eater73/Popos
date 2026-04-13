a9d2da6f9c6f5234276e77bde7b7d71b10d30060
Rachael <4hartrm@gmail.com>
2026-04-13 16:10:37 -0700

    Update WellFormed.java

--- chapter-01-homework-Crayon-eater73-5-main/chapter-01-homework-Crayon-eater73-5/WellFormed.java
+++ chapter-01-homework-Crayon-eater73-5-main/chapter-01-homework-Crayon-eater73-5/WellFormed.java
@@ -1,5 +1,6 @@
 public class WellFormed {
     public static void main (String[] args) {
         System.out.println ("A well-formed Java program has\n a main method with { and }\n braces.\n\n A system.out.println statement\n has ( and ) and usually a\n String that starts and ends\n with a \" charater.\n (But we can type \" instead!");
+        //The slash n is like breaking it into a new line. and slash quote is making a quotation mark
     }
 }
